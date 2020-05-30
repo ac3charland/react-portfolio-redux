fd450edf329f8e6d6e7738c2998e3b47d2dd8860
Alex Charland <ac3charland@gmail.com>
2020-05-30 16:11:52 -0500

    Update bio copy.

--- src/components/bio/bio.jsx
+++ src/components/bio/bio.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './bio.scss'
-import {HEADSHOT} from '../../utils/constants'
+import {HEADSHOT, BEST_BUY_URL, SWIFT_URL, BOOTCAMP_URL, MUSIC_URL, GITHUB_PROFILE_URL} from '../../utils/constants'
 
 const cb = 'bio'
 
 export default function Bio() {
     return (
-        <div id='bio-section' className={cb}>
+        <div className={cb}>
             <div className={`${cb}__content-wrapper`}>
                 <h2 className={`${cb}__heading`}>About</h2>
                 <div className={`${cb}__flex`}>
@@ -14,8 +14,9 @@ export default function Bio() {
                         <img className={`${cb}__image`} src={HEADSHOT} alt='Headshot of Alex Charland, Frontend Web Developer' />
                     </div>
                     <div className={`${cb}__copy-wrapper`}>
-                        <p>Direct trade hot chicken chillwave fixie, glossier put a bird on it stumptown humblebrag tote bag 90's palo santo. Beard literally locavore, tofu copper mug single-origin coffee slow-carb kogi butcher dreamcatcher fam irony knausgaard cardigan umami. Migas selfies succulents, kale chips affogato vegan small batch marfa iceland squid post-ironic sustainable subway tile readymade gluten-free. Leggings everyday carry next level plaid, flexitarian drinking vinegar VHS crucifix mixtape cornhole jianbing. Post-ironic franzen air plant ugh wayfarers. Next level taxidermy pork belly occupy cold-pressed cray.</p>
-                        <p>Heirloom shoreditch 90's chicharrones semiotics 3 wolf moon butcher franzen glossier paleo thundercats coloring book fixie meggings chia. Glossier YOLO gluten-free, leggings pinterest try-hard la croix copper mug banh mi. Cray jean shorts readymade normcore heirloom woke narwhal, tumeric etsy lo-fi 3 wolf moon next level cronut yuccie tbh. Poke la croix woke heirloom vice, banjo taxidermy asymmetrical keytar flannel +1 bushwick hot chicken green juice activated charcoal.</p>
+                        <p>Alex Charland is a frontend developer specializing in React, Redux, test-driven development, and accessible web design. He currently works building applications on the <a href={BEST_BUY_URL}>sixth-most-trafficked ecommerce site in the United States</a>. Driven and curious, with a love of collaboration and an eye for detail, he draws from experience in a variety of fields, including music, healthcare, and the biological sciences.</p>
+                        <p>After graduating from the University of Wisconsin Madison with a Bachelor of Science, Alex originally planned to be a physician assistant. However, after feeling burned out from his premed classes, he decided to take a gap year to try something completely different&mdash;coding. After spending a year teaching himself <a href={SWIFT_URL}>Swift</a>, Alex realized he had found his true calling, and quickly enrolled in the <a href={BOOTCAMP_URL}>University of Minnesota Coding Bootcamp</a>. There, he gained a solid footing in full-stack web development, including fundamentals like HTML, CSS, and Javascript, and modern technologies like React, Node.js, and MongoDB.</p>
+                        <p>From there, he landed his first position on a team at Best Buy, where he began his “second bootcamp”&mdash;this time learning Redux, CSS preprocessors (LESS), accessible web design, and test-driven development (including unit, integration, and visual regression testing). Outside of work, he loves <a href={MUSIC_URL}>playing and writing music</a>, cooking, running, and learning new technologies through the creation of various <a href={GITHUB_PROFILE_URL}>side projects</a>.</p>
                     </div>
                 </div>
             </div>
