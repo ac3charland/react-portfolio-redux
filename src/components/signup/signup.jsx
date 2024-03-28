b2ef68863b1251e2f82a5ec4a6961216dba60118
Alex Charland <ac3charland@gmail.com>
2024-03-28 16:25:50 -0500

    Fix email signup embed

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -8,7 +8,7 @@ const Signup = ({title = 'Join my mailing list!'}) => (
     <div className={`${cb}__wrapper`}>
         <h2 className={`${cb}__heading`}>{title}</h2>
         <h3 className={`${cb}__subheading`}>Get one email a week with show info, music recommendations, and more.</h3>
-        <div id='custom-substack-embed'></div>
+        <iframe title='Substack Signup' src='https://alexcharland.substack.com/embed' width='350' height='150' style={{border: '1px solid #EEE', borderRadius: '8px', background: 'white'}} frameBorder='0' scrolling='no'></iframe>
     </div>
 )
 
